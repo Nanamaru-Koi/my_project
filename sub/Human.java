495ef99da74ce759a0d231cd1280edccdd686b83
Nakamura-Yuhi <57391058+Nakamura-Yuhi@users.noreply.github.com>
2020-05-13 16:49:23 +0900

    Humanクラスの作成を行った。

---
+++ sub/Human.java
@@ -0,0 +1,16 @@
+package sub;
+
+public class Human{
+	
+	private String name;
+	private int age;
+	private int strength;
+	private const int MAX_STRENGTH = 100;
+	
+	public Human(String name, int age ){
+		this.name = name;
+		this.age = age;
+		this.strenght = MAX_STRENGTH;
+		System.out.prinltn("----ok----");
+	}
+}
