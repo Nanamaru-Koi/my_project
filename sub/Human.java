70792c4dd849a6aec77e3cb86aef43f74a7de1a2
Nakamura-Yuhi <57391058+Nakamura-Yuhi@users.noreply.github.com>
2020-05-13 16:54:28 +0900

    エラーの修正を行った。

--- sub/Human.java
+++ sub/Human.java
@@ -5,12 +5,12 @@ public class Human{
 	private String name;
 	private int age;
 	private int strength;
-	private const int MAX_STRENGTH = 100;
+	final private int MAX_STRENGTH = 100;
 	
 	public Human(String name, int age ){
 		this.name = name;
 		this.age = age;
-		this.strenght = MAX_STRENGTH;
-		System.out.prinltn("----ok----");
+		this.strength = MAX_STRENGTH;
+		System.out.println("----ok----");
 	}
 }
