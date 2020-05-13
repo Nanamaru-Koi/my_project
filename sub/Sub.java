9623645e81570968b7b33163003bd463649533d2
Nakamura-Yuhi <57391058+Nakamura-Yuhi@users.noreply.github.com>
2020-05-13 15:21:23 +0900

    test

--- sub/Sub.java
+++ sub/Sub.java
@@ -3,14 +3,12 @@ package sub;
 public class Sub{
 	
 	private String name;
-	private int age;
 	
-	public Sub(String name, int age){
+	public Sub(String name){
 		System.out.println("Subクラスのコンストラクタが呼び出されました。");
 		System.out.println("Sub Class's Construct has been called");
 		
 		this.name = name;	//nameをセット
-		this.age = age;
 	}
 	public void test(){
 		System.out.println("test");
@@ -20,7 +18,10 @@ public class Sub{
 		return name;
 	}
 	
-	public int getAge(){
-		return age;
+	public void test2(){
+		System.out.println("test2");
+	}
+	public void test3(){
+		System.out.println("test3testtest");
 	}
 }
