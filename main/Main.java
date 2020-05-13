cba0124d56de63f63ce0fe6ef47cb75a4f45fdb0
Nakamura-Yuhi <57391058+Nakamura-Yuhi@users.noreply.github.com>
2020-05-13 15:51:57 +0900

    Subインスタンスを作成する際の引数に年齢を追加した。

--- main/Main.java
+++ main/Main.java
@@ -4,7 +4,7 @@ import sub.Sub;
 public class Main{
 	public static void main(String[] args ){
 		System.out.println("Hello World!");
-		Sub sub = new Sub("test");
+		Sub sub = new Sub("test",23);
 		sub.test();
 		System.out.println(sub.getName());
 	}
