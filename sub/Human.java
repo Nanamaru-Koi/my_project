c0690e8bdb4710ab18bdd51287738fae11e9cc40
Nakamura-Yuhi <57391058+Nakamura-Yuhi@users.noreply.github.com>
2020-05-13 17:08:27 +0900

    setAgeメソッドを追加した。

--- sub/Human.java
+++ sub/Human.java
@@ -17,4 +17,5 @@ public class Human{
 	public String getName(){ return name; }
 	public int getAge(){ return age; }
 	public void setName(String name){ this.name = name; }
+	public void setAge(int age ){ this.age = age; }
 }
